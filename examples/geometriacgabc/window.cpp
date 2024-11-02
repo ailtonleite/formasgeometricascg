4fe2a0212588b27d9aedfcdd4a40d6913d16070b
pedrohgsilva <pedro.garcez@aluno.ufabc.edu.br>
2024-11-01 21:26:14 -0300

    Update window.cpp

--- examples/geometriacgabc/window.cpp
+++ examples/geometriacgabc/window.cpp
@@ -1,66 +1,91 @@
 #include "window.hpp"
 
 void Window::onCreate() {
-  // TODO: Ao iniciar a aplicacao, pré-selecionar a primeira forma geometrica (primeiro botão)
+    // Pré-selecionar a primeira forma geométrica (primeiro botão)
+    m_selectedShapeIndex = 0; // Assume que m_selectedShapeIndex é um membro da classe
 }
 
 void Window::onPaintUI() {
-  // Get size of application's window
-  auto const appWindowWidth{gsl::narrow<float>(getWindowSettings().width)};
-  auto const appWindowHeight{gsl::narrow<float>(getWindowSettings().height)};
+    // Get size of application's window
+    auto const appWindowWidth{gsl::narrow<float>(getWindowSettings().width)};
+    auto const appWindowHeight{gsl::narrow<float>(getWindowSettings().height)};
 
-  // Janela dos botões
-  {
-    // Comprimeto da tela / 3 de forma que os botões ocupem apenas 1/3 da tela
-    ImGui::SetNextWindowSize(ImVec2(appWindowWidth, appWindowHeight/3));
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-
-    auto const flags{ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoResize |
-                     ImGuiWindowFlags_NoCollapse};
-    ImGui::Begin("Geometria ABC", nullptr, flags);
-
-    // TODO: Add botões e texto nos botões -- FEITO
+    // Janela dos botões
     {
-        auto const gridHeight{(appWindowHeight/3) - 22 - 20 - (m_l * 10) - 20};
+        ImGui::SetNextWindowSize(ImVec2(appWindowWidth, appWindowHeight / 3));
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+
+        auto const flags{ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoResize |
+                         ImGuiWindowFlags_NoCollapse};
+        ImGui::Begin("Geometria ABC", nullptr, flags);
+
+        auto const gridHeight{(appWindowHeight / 3) - 22 - 20 - (m_l * 10) - 20};
         auto const buttonHeight{gridHeight / m_l};
 
-        // Texto formas geometricas nos botões
-        std::array<std::string, m_l*m_c> const fg = {
-            "palavra1", 
-            "palavra2", 
-            "palavra3", 
-            "palavra4", 
-            "palavra5", 
-            "palavra6"};
+        std::array<std::string, m_l * m_c> const fg = {
+            "Forma 1", 
+            "Forma 2", 
+            "Forma 3", 
+            "Forma 4", 
+            "Forma 5", 
+            "Forma 6"
+        };
 
-        // Use custom font
         if (ImGui::BeginTable("buttons", m_c)) {
             for (auto i : iter::range(m_l)) {
-            ImGui::TableNextRow();
-            for (auto j : iter::range(m_c)) {
-                ImGui::TableSetColumnIndex(j);
-                auto const offset = i * m_c + j;
+                ImGui::TableNextRow();
+                for (auto j : iter::range(m_c)) {
+                    ImGui::TableSetColumnIndex(j);
+                    auto const offset = i * m_c + j;
 
-                auto const& fg_value = fg[offset];
+                    auto const& fg_value = fg[offset];
 
-                // Button text is fg followed by an ID in the format ##ij
-                auto buttonText{fmt::format("{}##{}{}", fg_value, i, j)}; // --
-                if (ImGui::Button(buttonText.c_str(), ImVec2(-1, buttonHeight))) {
-                    m_button.at(offset) = fg_value;
+                    auto buttonText{fmt::format("{}##{}{}", fg_value, i, j)};
+                    if (ImGui::Button(buttonText.c_str(), ImVec2(-1, buttonHeight))) {
+                        m_button.at(offset) = fg_value;
+                        m_selectedShapeIndex = offset; // Armazena o índice do botão pressionado
+                    }
                 }
+                ImGui::Spacing();
             }
-            ImGui::Spacing();
+            ImGui::EndTable();
         }
-        ImGui::EndTable();
-      }
+
+        ImGui::Spacing();
     }
 
-    ImGui::Spacing();
-    }
-    
-    // TODO: Add exibir qual botão está selecionado
-    // TODO: Add formas geometricas
-    // TODO: Add janela que mostrará as formas geometricas
+    // Exibir qual botão está selecionado
+    ImGui::Text("Forma selecionada: %s", fg[m_selectedShapeIndex].c_str());
+
+    // Desenhar a forma geométrica correspondente
+    drawShape(m_selectedShapeIndex); // Função para desenhar a forma
 
     ImGui::End();
+}
+
+void Window::drawShape(int index) {
+    // Aqui você deve implementar a lógica para desenhar a forma correspondente ao índice
+    // Por exemplo:
+    switch (index) {
+        case 0:
+            // Desenhar Forma 1
+            break;
+        case 1:
+            // Desenhar Forma 2
+            break;
+        case 2:
+            // Desenhar Forma 3
+            break;
+        case 3:
+            // Desenhar Forma 4
+            break;
+        case 4:
+            // Desenhar Forma 5
+            break;
+        case 5:
+            // Desenhar Forma 6
+            break;
+        default:
+            break;
+    }
 }
